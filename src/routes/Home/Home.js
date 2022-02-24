54641b0d61f2fff3a0cfa609a5c04b38681b612b
Aaron Mcnulty <amcnulty@apttus.com>
2022-02-24 10:58:42 -0600

    Landing Page Work

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -1,50 +1,109 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { loadTopCategories } from '../../features/categories/categoriesSlice';
 import { loadProducts } from '../../features/products/productsSlice';
 import './Home.scss';
 
 const Home = () => {
   const dispatch = useDispatch();
   const products = useSelector(state => state.products);
+  const categories = useSelector(state => state.categories);
 
   useEffect(() => {
     dispatch(loadProducts());
+    dispatch(loadTopCategories())
   }, [dispatch]);
 
   return (
-    <div className='Home' style={{
-      backgroundImage: `url(https://picsum.photos/1920/1080)`,
-      backgroundAttachment: 'fixed'
-    }}>
-      <div className='landing d-flex'>
+    <div
+      className='Home'
+      style={{
+        backgroundImage: `url(https://picsum.photos/1920/1080)`,
+        backgroundAttachment: 'fixed'
+      }}
+    >
+      <div
+        className='landing d-flex'
+        style={{
+          backgroundImage: `url(https://picsum.photos/1920/400)`
+        }}
+      >
         <div className='leftContent d-flex ms-auto me-5 w-50 py-4 align-self-center justify-content-center'>
           <div className='d-inline-block'>
-            <h3 className='fancyLetters'>Shop Our <br/>Unique<br/>Products</h3>
-            <Link className='btn btn-outline-primary mt-4 w-100 text-decoration-none' to='/catalog'>Shop Now</Link>
+            <h3 className='fancyLetters'>
+              Shop Our <br />
+              Unique
+              <br />
+              Products
+            </h3>
+            <Link
+              className='btn btn-outline-primary mt-4 w-100 text-decoration-none'
+              to='/catalog'
+            >
+              Shop Now
+            </Link>
           </div>
+        </div>
+      </div>
+      <div className='categoriesSection py-5'>
+        <div className='row justify-content-around'>
+          {
+            categories.list.map((category, index) => (
+              <div className={`col-11 my-4 col-md-6 col-lg-4 ${index > 2 && 'd-lg-none'}`} key={category.Id}>
+                <div className='categoryCard card p-5'>
+                  <h4 className='fw-bold'>{category.Label}</h4>
+                  <img className='p-5' src={category.ImageURL} alt={category.Label} />
+                  <p>{category.Description}</p>
+                  <div className='d-flex justify-content-center'>
+                    <Link to='/catalog' className='btn btn-outline-secondary w-50'>Shop {category.Label}</Link>
+                  </div>
+                </div>
+              </div>
+            ))
+          }
         </div>
       </div>
       <div className='aboutSection py-5 px-3'>
         <h2 className='text-center mb-5 fw-bold text-light'>As Seen In</h2>
         <div className='row justify-content-center'>
           <div className='col-6 col-md-3 col-lg-2 mb-5 text-center'>
-            <img className='' src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/The_Huffington_Post_logo.svg/1200px-The_Huffington_Post_logo.svg.png' alt='HuffPost'/>
+            <img
+              className=''
+              src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/The_Huffington_Post_logo.svg/1200px-The_Huffington_Post_logo.svg.png'
+              alt='HuffPost'
+            />
           </div>
           <div className='col-6 col-md-3 col-lg-2 mb-5 text-center'>
-            <img className='' src='https://1000logos.net/wp-content/uploads/2016/11/CNN-Logo-1980.png' alt='HuffPost'/>
+            <img
+              className=''
+              src='https://1000logos.net/wp-content/uploads/2016/11/CNN-Logo-1980.png'
+              alt='HuffPost'
+            />
           </div>
           <div className='col-6 col-md-3 col-lg-2 mb-5 text-center'>
-            <img className='' src='https://cdn.iconscout.com/icon/free/png-256/gq-282934.png' alt='HuffPost'/>
+            <img
+              className=''
+              src='https://cdn.iconscout.com/icon/free/png-256/gq-282934.png'
+              alt='HuffPost'
+            />
           </div>
           <div className='col-6 col-md-3 col-lg-2 mb-5 text-center'>
-            <img className='' src='https://1000logos.net/wp-content/uploads/2021/05/Forbes-logo.png' alt='HuffPost'/>
+            <img
+              className=''
+              src='https://1000logos.net/wp-content/uploads/2021/05/Forbes-logo.png'
+              alt='HuffPost'
+            />
           </div>
         </div>
         <div className='row justify-content-around'>
           <figure className='col-11 col-md-5 text-center'>
             <blockquote className='blockquote'>
-              <p>&ldquo;An excellent company to work with! We marvel at the high quality and great customer service. Look forward to years of continued support from us.&rdquo;</p>
+              <p>
+                &ldquo;An excellent company to work with! We marvel at the high
+                quality and great customer service. Look forward to years of
+                continued support from us.&rdquo;
+              </p>
               <figcaption className='blockquote-footer'>
                 Arthur Morgan <cite>Stars and Stripes Logistics</cite>
               </figcaption>
@@ -52,7 +111,11 @@ const Home = () => {
           </figure>
           <figure className='col-11 col-md-5 text-center'>
             <blockquote className='blockquote'>
-              <p>&ldquo;Easy to work with an has great integration with our products and services. We enjoy the time saved working with the streamlined processes.&rdquo;</p>
+              <p>
+                &ldquo;Easy to work with an has great integration with our
+                products and services. We enjoy the time saved working with the
+                streamlined processes.&rdquo;
+              </p>
               <figcaption className='blockquote-footer'>
                 Rufus Shinra <cite>Shinra Energy Corp.</cite>
               </figcaption>
@@ -63,47 +126,114 @@ const Home = () => {
       <div className='featuredProductsList d-flex justify-content-center'>
         <div className='bg-transparent'>
           <div className='customCard'>
-            {
-              products.list.length > 0
-              &&
+            {products.list.length > 0 && (
               <div className='featuredProductCard px-5'>
                 <h3 className='fw-bold'>{products.list[0].Name}</h3>
-                <img className='py-5 w-100' src={products.list[0].ImageURL} alt='featured product'/>
+                <img
+                  className='py-5 w-100'
+                  src={products.list[0].ImageURL}
+                  alt='featured product'
+                />
                 <p className='fst-italic'>{products.list[0].Description}</p>
                 <div className='d-flex justify-content-center'>
                   <div className='w-50 text-end'>
-                    <p className='fw-bold h5 strike text-danger'><del>$249</del></p>
+                    <p className='fw-bold h5 strike text-danger'>
+                      <del>$249</del>
+                    </p>
                     <p className='fw-bold h5'>$199</p>
-                    <Link to='/catalog' className='btn btn-outline-secondary w-100 text-decoration-none'>BUY NOW</Link>
+                    <Link
+                      to='/catalog'
+                      className='btn btn-outline-secondary w-100 text-decoration-none'
+                    >
+                      BUY NOW
+                    </Link>
                   </div>
                 </div>
               </div>
-            }
+            )}
           </div>
           <div className='customCard'>
-            {
-              products.list.length > 0
-              &&
+            {products.list.length > 0 && (
               <div className='featuredProductCard px-5'>
                 <h3 className='fw-bold'>{products.list[1].Name}</h3>
-                <img className='py-5 w-100' src={products.list[1].ImageURL} alt='featured product'/>
+                <img
+                  className='py-5 w-100'
+                  src={products.list[1].ImageURL}
+                  alt='featured product'
+                />
                 <p className='fst-italic'>{products.list[1].Description}</p>
                 <div className='d-flex justify-content-center'>
                   <div className='w-50 text-end'>
-                    <p className='fw-bold h5 strike text-danger'><del>$249</del></p>
+                    <p className='fw-bold h5 strike text-danger'>
+                      <del>$249</del>
+                    </p>
                     <p className='fw-bold h5'>$199</p>
-                    <Link to='/catalog' className='btn btn-outline-secondary w-100 text-decoration-none'>BUY NOW</Link>
+                    <Link
+                      to='/catalog'
+                      className='btn btn-outline-secondary w-100 text-decoration-none'
+                    >
+                      BUY NOW
+                    </Link>
                   </div>
                 </div>
               </div>
-            }
+            )}
           </div>
-          <div className='customCard'>
-            <p className='h2 p-5 text-secondary fw-bold text-uppercase'>WHAT MAKES US SO GREAT Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum itaque debitis ad nesciunt repudiandae est repellendus quae consectetur sit fugiat!</p>
+          <div className='customCard textCard'>
+            <p className='bigText text-white h2 p-5 text-secondary fw-bold text-uppercase'>
+              <span className='h1'>W</span>HAT MAKES US <span className='text-danger'>SO GREAT</span> Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. <i>Ipsum itaque</i> debitis ad nesciunt repudiandae est
+              repellendus quae consectetur <span className='text-primary'>sit fugiat!</span>
+            </p>
           </div>
-          <div className='customCard'>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis consequuntur doloremque a ad, perspiciatis magni facere, molestiae pariatur unde rerum suscipit et expedita velit quisquam voluptatem fugiat, nam hic recusandae aspernatur ipsam. Fugiat sint iure omnis. Voluptas vitae dolore, vel dolor neque sit, nemo aut molestiae natus ipsum doloremque! Laboriosam asperiores adipisci possimus error ut earum modi vitae architecto quia in harum aspernatur est quisquam quo voluptate eius repellendus impedit sint tenetur expedita ad, voluptates commodi quod perferendis. Illum accusamus soluta qui facilis voluptatum sequi, culpa voluptatibus voluptatem, fugit maiores modi porro? Dolores culpa facere quidem consequuntur iusto rerum iure?</p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis sit quis quaerat possimus odio repellat tempora, ipsa fugiat nobis eaque nesciunt officiis velit praesentium et sequi nostrum molestiae dolorem consequatur.</p>
+          <div className='customCard footerCard p-5'>
+            <h5>Conga eCommerce ROC</h5>
+            <div className='row justify-content-around'>
+              <div className='col-5'>
+                <b className='d-block mt-4'>OUR SITE</b>
+                <div className='ms-3'>
+                  <Link to='/' className='text-decoration-none'>Home</Link>
+                </div>
+                <div className='ms-3'>
+                  <Link to='/catalog' className='text-decoration-none'>Catalog</Link>
+                </div>
+                <div className='ms-3'>
+                  <Link to='/catalog' className='text-decoration-none'>Products</Link>
+                </div>
+                <div className='ms-3'>
+                  <Link to='/catalog' className='text-decoration-none'>Settings</Link>
+                </div>
+              </div>
+              <div className='col-5'>
+                <b className='d-block mt-4'>CONTACT</b>
+                <div className='ms-3'>
+                  <Link to='/' className='text-decoration-none'>Email</Link>
+                </div>
+                <div className='ms-3'>
+                  <Link to='/catalog' className='text-decoration-none'>Address Info</Link>
+                </div>
+                <b className='d-block mt-4'>FOLLOW</b>
+                <div className='ms-3'>
+                  <Link to='/' className='text-decoration-none'>Twitter</Link>
+                </div>
+                <div className='ms-3'>
+                  <Link to='/catalog' className='text-decoration-none'>Instagram</Link>
+                </div>
+                <div className='ms-3'>
+                  <Link to='/catalog' className='text-decoration-none'>YouTube</Link>
+                </div>
+                <div className='ms-3'>
+                  <Link to='/catalog' className='text-decoration-none'>Facebook</Link>
+                </div>
+                <div className='ms-3'>
+                  <Link to='/catalog' className='text-decoration-none'>CodePen</Link>
+                </div>
+                <div className='ms-3'>
+                  <Link to='/catalog' className='text-decoration-none'>GitHub</Link>
+                </div>
+              </div>
+            </div>
+            <p className='mt-5 mb-0 text-center'>&copy; Conga eCommerce ROC 2022</p>
           </div>
         </div>
       </div>
